a37c78de9aff84869cac6a289237fee2a9e07621
Kenneth Barker <krbarker@gmail.com>
2016-12-20 15:27:36 -0500

    Modified main activity to add a new method Added TestClass

--- app/src/main/java/com/timbuchalka/calculator/MainActivity.java
+++ app/src/main/java/com/timbuchalka/calculator/MainActivity.java
@@ -168,4 +168,7 @@ public class MainActivity extends AppCompatActivity {
         result.setText(operand1.toString());
         newNumber.setText("");
     }
+    public String returnName() {
+        return "Ken was here";
+    }
 }
---
+++ app/src/main/java/com/timbuchalka/calculator/TestClass.java
@@ -0,0 +1,8 @@
+package com.timbuchalka.calculator;
+
+/**
+ * Created by kbx on 12/20/16.
+ */
+
+public class TestClass {
+}
